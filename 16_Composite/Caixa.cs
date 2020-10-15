3a606aa4e347aa975699d4aa98f48c1c41e59c30
Ricardo Mendes <rmcs87@gmail.com>
2020-10-14 22:19:34 -0300

    Composite - Final

--- 16_Composite/Caixa.cs
+++ 16_Composite/Caixa.cs
@@ -2,9 +2,43 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace _16_Composite
+public class Caixa : IItem
 {
-    class Caixa
+    private List<IItem> _items;
+
+    public Caixa()
     {
+        _items = new List<IItem>();
+    }
+
+    public List<IItem> ItemList { get { return _items; } }
+
+    public float GetPrice()
+    {
+        float value = 0;
+        foreach (var item in _items)
+        {
+            value += item.GetPrice();
+        }
+        return value + 0.5f;
+    }
+    public float GetVolume()
+    {
+        float value = 0;
+        foreach (var item in _items)
+        {
+            value += item.GetVolume();
+        }
+        if(value < 5)
+        {
+            return 5;//caixa pequena
+        }else if (value < 10)
+        {
+            return 10;//caixa media
+        }
+        else
+        {
+            return 100;//caxa grande
+        }
     }
 }
--- 16_Composite/Program.cs
+++ 16_Composite/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("Composite");
+
+        Caixa cmenor = new Caixa();
+        cmenor.ItemList.Add(new Martelo()); 
+        cmenor.ItemList.Add(new Martelo()); 
+        cmenor.ItemList.Add(new Martelo()); 
+        cmenor.ItemList.Add(new Martelo());
+
+        Caixa cmaior = new Caixa();
+        cmaior.ItemList.Add(cmenor);
+        cmaior.ItemList.Add(new Celular());
+        cmaior.ItemList.Add(new Celular());
+        cmaior.ItemList.Add(new Celular());
+        cmaior.ItemList.Add(new Martelo());
+
+        Console.WriteLine($"Custo = > {cmaior.GetPrice()}");
+        Console.WriteLine($"Volume = > {cmaior.GetVolume()}");
+
     }
 }
