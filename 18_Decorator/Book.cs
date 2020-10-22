a9f98ae975c8d4e12aa760164c14c8486a76af40
Ricardo Mendes <rmcs87@gmail.com>
2020-10-22 20:21:06 -0300

    Decorator - Componente Concreto

---
+++ 18_Decorator/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Book : LibraryItem
+{
+    public Book(string title, string author, int copies)
+    {
+        Title = title;
+        Author = author;
+        NumCopies = copies;
+    }
+
+    public string Title { get; set; }
+    public string Author { get; set; }
+
+    public override void Display()
+    {
+        Console.WriteLine($"Book");
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"#Copies : {NumCopies}");
+    }
+}
