b67fff70edd70ddf528ee5d0c784620a7963f9f4
Ricardo Mendes <rmcs87@gmail.com>
2020-10-21 22:12:31 -0300

    Facade - Facade

--- 17_Facade/FacadeCheckuser.cs
+++ 17_Facade/FacadeCheckuser.cs
@@ -2,9 +2,31 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace _17_Facade
+
+public class FacadeCheckuser
 {
-    class FacadeCheckuser
+    private Bank subSystemBank;
+    private Credit subSystemCredit;
+    private Loan subSystemLoan;
+    public FacadeCheckuser()
     {
+        subSystemBank = new Bank();
+        subSystemCredit = new Credit();       
+        subSystemLoan = new Loan();
+    }
+    public bool IsEligible(Client c, int amount)
+    {
+        Console.WriteLine($"Solicitação de Emprestimo por: {c.Name}:");
+        if (!subSystemBank.CheckSavings(c, amount) 
+            || !subSystemCredit.CheckClenaNameInSerasa(c)
+            || subSystemLoan.CheckForOtherLoans(c))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 }
+
