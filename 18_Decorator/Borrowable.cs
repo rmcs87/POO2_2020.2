c1c753e3de8a0c3154f56d6f68d6e96b4c6007e4
Ricardo Mendes <rmcs87@gmail.com>
2020-10-22 20:32:40 -0300

    Decorator - Concrete Decorator

---
+++ 18_Decorator/Borrowable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+public class Borrowable : Decorator
+{
+    protected List<string> borrowers = new List<string>();
+    public Borrowable(LibraryItem libraryItem)
+      : base(libraryItem)
+    {
+    }
+    public void BorrowItem(string name)
+    {
+        borrowers.Add(name);
+        _libraryItem.NumCopies--;
+    }
+    public void ReturnItem(string name)
+    {
+        borrowers.Remove(name);
+        _libraryItem.NumCopies++;
+    }
+    public override void Display()
+    {
+        base.Display();
+        foreach (string borrower in borrowers)
+        {
+            Console.WriteLine(" borrower: " + borrower);
+        }
+    }
+}
---
+++ 18_Decorator/Decorator.cs
@@ -0,0 +1,14 @@
+﻿public abstract class Decorator : LibraryItem
+{
+    protected LibraryItem _libraryItem;
+
+	public Decorator(LibraryItem item)
+	{
+		_libraryItem = item;
+	}
+
+	public override void Display()
+	{
+		_libraryItem.Display();
+	}
+}
