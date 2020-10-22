fe97e1ad03ddbf2095fc2088516874cc4385d0af
Ricardo Mendes <rmcs87@gmail.com>
2020-10-22 20:37:59 -0300

    Decorator - Program

--- 18_Decorator/Program.cs
+++ 18_Decorator/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 
-namespace _18_Decorator
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Console.WriteLine("DECORATOR !!!!");
+
+        Book book = new Book("C#", "Desconhecido", 5);
+        book.Display();
+
+        Console.WriteLine("Mudando o comportamento.......");
+
+        Borrowable borrowableBook = new Borrowable(book);
+
+        borrowableBook.BorrowItem("Fulano");
+        borrowableBook.BorrowItem("Sicrano");
+
+        borrowableBook.Display();
+
     }
 }
