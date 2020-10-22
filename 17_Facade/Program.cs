f0f5afc69e8cf2a131467cef87f294a30da9c5f7
Ricardo Mendes <rmcs87@gmail.com>
2020-10-21 22:18:39 -0300

    Facade - end

--- 17_Facade/Program.cs
+++ 17_Facade/Program.cs
@@ -5,5 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Facade Pattern!");
+
+        Client client = new Client("Ricardo Mendes");
+
+        FacadeCheckuser facade = new FacadeCheckuser();
+
+        bool emprestar = facade.IsEligible(client, 2500);
+
+        if (emprestar)
+        {
+            Console.WriteLine("Conceder Emprestimo");
+        }
+        else
+        {
+            Console.WriteLine("Não Conceder Emprestimo");
+        }
     }
 }
