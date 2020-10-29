adbbba0e79c2b92f4e389bef3fe82e68888ca7ce
Ricardo Mendes <rmcs87@gmail.com>
2020-10-28 22:04:20 -0300

    Proxy - Proxy

---
+++ 19_Proxy/MathService.cs
@@ -0,0 +1,19 @@
+﻿public class MathService : IMath
+{
+    public double Add(double x, double y)
+    {
+        return x + y;
+    }
+    public double Div(double x, double y)
+    {
+        return x / y;
+    }
+    public double Mul(double x, double y)
+    {
+        return x * y;
+    }
+    public double Sub(double x, double y)
+    {
+        return x - y;
+    }
+}
---
+++ 19_Proxy/ProxyMath.cs
@@ -0,0 +1,40 @@
+﻿using System;
+public class ProxyMath : IMath
+{
+    private MathService _math;
+    public ProxyMath(MathService math)
+    {
+        _math = math;
+    }
+    public double Add(double x, double y)
+    {
+        if (x < 0 || y < 0)
+            throw new Exception("Number must be positive.");
+
+        return _math.Add(x,y);
+    }
+
+    public double Div(double x, double y)
+    {
+        if (x < 0 || y < 0)
+            throw new Exception("Number must be positive.");
+
+        return _math.Div(x, y);
+    }
+
+    public double Mul(double x, double y)
+    {
+        if (x < 0 || y < 0)
+            throw new Exception("Number must be positive.");
+
+        return _math.Mul(x, y);
+    }
+
+    public double Sub(double x, double y)
+    {
+        if (x < 0 || y < 0)
+            throw new Exception("Number must be positive.");
+
+        return _math.Sub(x, y);
+    }
+}
