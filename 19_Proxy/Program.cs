f518a003e028075cca34c07620299c8e11c22792
Ricardo Mendes <rmcs87@gmail.com>
2020-10-28 21:53:52 -0300

    Proxy - Base

---
+++ 19_Proxy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _19_Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Proxy!");
+        }
+    }
+}
