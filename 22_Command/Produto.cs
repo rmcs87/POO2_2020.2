e4f0f869af909f365c631bec26b5367509954b5f
Ricardo Mendes <rmcs87@gmail.com>
2020-11-05 20:09:53 -0300

    Command - Receiver

---
+++ 22_Command/Produto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//Classe Receiver
+public class Produto
+{
+    public Produto(string name, decimal preco)
+    {
+        Name = name;
+        Preco = preco;
+    }
+    public string Name { get; set; }
+    public decimal Preco { get; set; }
+    public void AumentarPreco(decimal qtd)
+    {
+        Preco += qtd;
+        Console.WriteLine($"O Preço para {Name} aumentou em {qtd}");
+    }
+    public void DiminuirPreco(decimal qtd)
+    {
+        if (Preco > qtd)
+        {
+            Preco -= qtd;
+            Console.WriteLine($"O Preço para {Name} diminuiu em {qtd}");
+        }
+    }
+    public override string ToString()
+    {
+        return $"Valor atual do{Name} é {Preco} ";
+    }
+}
---
+++ 22_Command/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _22_Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
